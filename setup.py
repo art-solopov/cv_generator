5ed0d5077ca9d9db51094c9c99aa000e262e783a
Artemiy Solopov <art-solopov@yandex.ru>
2016-04-27 00:53:40 +0300

    Started the generator

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CV generator',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Jinja2 >=2.8, <3',
+        'click >=6, <7',
+        'PyYAML'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cv_generator = cv_generator.cmd:generate'
+        ]
+    }
+)
+
