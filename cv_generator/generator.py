cd1afdd8c6cf950af38aa6051af757b5fcc68ba5
Artemiy Solopov <art-solopov@yandex.ru>
2016-06-01 01:28:58 +0300

    Data loading

--- cv_generator/generator.py
+++ cv_generator/generator.py
@@ -1,6 +1,7 @@
 import jinja2
 import yaml
 import os
+import pathlib
 
 LATEX_JINJA_PARAMS = dict(
     block_start_string='\BLOCK{',
@@ -20,3 +21,16 @@ TEMPLATES = ['cv.tex']
 def process(configuration, data_dir, template_dir):
     loader = jinja2.FileSystemLoader(os.path.abspath(template_dir))
     env = jinja2.Environment(loader=loader, **LATEX_JINJA_PARAMS)
+    data = _load_data(data_dir)
+
+def _load_data(data_dir):
+    data = {}
+    data_root = pathlib.Path(data_dir)
+    data_paths = [x for x in data_root.iterdir()
+                  if x.suffix == '.yml' or x.suffix == '.yaml']
+    for path in data_paths:
+        with open(str(path), 'r') as df:
+            file_data = yaml.load(df)
+            data[path.stem] = file_data
+
+    return data
