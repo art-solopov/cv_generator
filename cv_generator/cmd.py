312687afd41232ca128fbd9f912c5955f6afc3df
Artemiy Solopov <art-solopov@yandex.ru>
2016-06-17 00:18:58 +0300

    Started the command

--- cv_generator/cmd.py
+++ cv_generator/cmd.py
@@ -1,5 +1,12 @@
 import click
+import configparser
+
+from cv_generator.generator import process
 
 @click.command()
-def generate():
-    pass
+@click.option('--config', default='cv_generation.ini', help='Configuration file')
+@click.argument('templatedir')
+@click.argument('datadir')
+def generate(config_file, template_dir, data_dir):
+    cfg = configparser.ConfigParser().read(config_file)
+    process(configuration=cfg, data_dir=data_dir, template_dir=template_dir)
