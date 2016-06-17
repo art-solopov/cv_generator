0aaeb15b24116b059315ecaae1eeb0ac76817178
Artemiy Solopov <art-solopov@yandex.ru>
2016-06-18 02:08:48 +0300

    Implemented the generator

--- cv_generator/generator.py
+++ cv_generator/generator.py
@@ -1,14 +1,19 @@
+import os
+from os.path import join as pjoin
+import tempfile
+import pathlib
+import subprocess as sp
+import shutil as sh
+
 import jinja2
 import yaml
-import os
-import pathlib
 
 LATEX_JINJA_PARAMS = dict(
-    block_start_string='\BLOCK{',
+    block_start_string=r'\BLOCK{',
     block_end_string='}',
-    variable_start_string='\VAR{',
+    variable_start_string=r'\VAR{',
     variable_end_string='}',
-    comment_start_string='\#{',
+    comment_start_string=r'\#{',
     comment_end_string='}',
     line_statement_prefix='%%',
     line_comment_prefix='%#',
@@ -16,12 +21,37 @@ LATEX_JINJA_PARAMS = dict(
     autoescape=False,
 )
 
-TEMPLATES = ['cv.tex']
-
 def process(configuration, data_dir, template_dir):
     loader = jinja2.FileSystemLoader(os.path.abspath(template_dir))
     env = jinja2.Environment(loader=loader, **LATEX_JINJA_PARAMS)
     data = _load_data(data_dir)
+
+    latex_config = configuration.get('latex', {})
+    latex_program = latex_config.get('program', 'latex')
+    latex_texfile = latex_config.get('root', 'index.tex')
+    latex_format = latex_config.get('format', 'dvi')
+
+    template_root = pathlib.Path(template_dir)
+    with tempfile.TemporaryDirectory(prefix='cv_generator') as result_dir:
+        for template_path in template_root.iterdir():
+            if template_path.suffix != '.tex' or template_path.name.startswith('.'):
+                continue
+            template = env.get_template(template_path.name)
+            result_path = pjoin(result_dir, template_path.name)
+            with open(result_path, 'w') as resfile:
+                resfile.write(template.render(**data))
+            sp.call([
+                latex_program, '-output-directory', result_dir,
+                pjoin(result_dir, latex_texfile)
+            ])
+            sh.copy(
+                pjoin(
+                    result_dir,
+                    latex_texfile.replace('.tex', '.' + latex_format)
+                ),
+                os.getcwd()
+            )
+
 
 def _load_data(data_dir):
     data = {}
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,9 +1,12 @@
 import unittest
-import cv_generator.generator
-from tempfile import TemporaryDirectory, TemporaryFile
+from tempfile import TemporaryDirectory
+import os
 from os.path import join as pjoin, getsize as filesize
-from yaml import dump as ymldump
 from textwrap import dedent
+
+from yaml import dump as ymldump
+
+import cv_generator.generator
 
 class CVGeneratorTest(unittest.TestCase):
     """Testing the CVGenerator class"""
@@ -14,24 +17,34 @@ class CVGeneratorTest(unittest.TestCase):
     }
 
     TEST_TEMPLATE = dedent('''\
-    \documentclass[12pt]{article}
-    \title{\VAR{data.title}}
-    \begin{document}
-    \VAR{data.body}
-    \end{document}
+    \\documentclass[12pt]{article}
+    \\title{\\VAR{data.title}}
+    \\begin{document}
+    \\VAR{data.body}
+    \\end{document}
     ''')
 
     def setUp(self):
+        self._cwd = os.getcwd()
         self.data_dir = TemporaryDirectory()
         self.template_dir = TemporaryDirectory()
-        self.out_file = TemporaryFile(mode='r', suffix='.pdf')
+        self.tempwd = TemporaryDirectory()
         self.configuration = {
-            'out_file': self.out_file.name
+            'latex': {
+                'program': 'pdflatex',
+                'root': 'main.tex',
+                'format': 'pdf'
+            }
         }
+        self.out_file = pjoin(self.tempwd.name, 'main.pdf')
         with open(pjoin(self.data_dir.name, 'data.yml'), 'w') as data_file:
             ymldump(self.TEST_DATA, stream=data_file)
         with open(pjoin(self.template_dir.name, 'main.tex'), 'w') as template_file:
             template_file.write(self.TEST_TEMPLATE)
+        os.chdir(self.tempwd.name)
+
+    def tearDown(self):
+        os.chdir(self._cwd)
 
     def test_process(self):
         cv_generator.generator.process(
@@ -39,5 +52,4 @@ class CVGeneratorTest(unittest.TestCase):
             data_dir=self.data_dir.name,
             template_dir=self.template_dir.name
         )
-        self.assertGreater(filesize(self.out_file.name), 0)
-        self.fail('Incomplete test')
+        self.assertGreater(filesize(self.out_file), 0)
